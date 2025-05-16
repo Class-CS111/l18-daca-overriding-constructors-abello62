c3495291cc0dc389fe340acf76c49da13fc792a0
abello62 <abellojr.62@gmail.com>
2025-05-16 03:25:01 +0000

    LAB 18

--- DACArecipient.java
+++ DACArecipient.java
@@ -46,6 +46,8 @@
 
 public class DACArecipient
 {
+
+	public static final String DEFAULT_SURNAME = "Unkown";
 	/***** INSTANCE VARIABLES *****/
   private String surname;
   private String givenName;
@@ -56,10 +58,120 @@ public class DACArecipient
 	
 
 	//TODO: don't forget to copy all your methods from the previous labs!
-	
+
+  public String getSurname() 
+	{
+		return this.surname;
+	}
+	public String getGivenName()
+	{
+		return this.givenName;
+	}
+	public String getUscisNumber()
+	{
+		return this.uscisNumber;
+	}
+	public String getCountryOfOrigin()
+	{
+		return this.countryOfOrigin;		
+	}
+	public int getBirthday()
+	{
+		return this.birthday;
+	}
+	public int getValidFromDate()
+	{
+		return this.validFromDate;
+	}
+	public int getExpirationDate()
+	{
+		return this.expirationDate;
+	}
+	public char getSex()
+	{
+		return this.sex;
+	}
+
+
+  public void setSurname(String surname) 
+	{
+		this.surname = surname; 
+	}
+	public void setGivenName(String givenName)
+	{
+		this.givenName = givenName; 
+	}
+	public void setUscisNumber(String uscisNumber)
+	{
+		this.uscisNumber = uscisNumber;
+	}
+	public void setCountryOfOrigin(String countryOfOrigin)
+	{
+		this.countryOfOrigin = countryOfOrigin;
+	}
+	public void setBirthday(int birthday)
+	{
+		this.birthday = birthday;
+	}
+	public void setValidFromDate(int validFromDate)
+	{
+		this.validFromDate = validFromDate;
+	}
+	public void setExpirationDate (int expirationDate)
+	{
+		this.expirationDate = expirationDate;
+	}
+	public void setSex (char sex)
+	{
+		this.sex = sex; 
+	}
+
+  public void setAll(String surname, String givenName, String uscisNumber, 
+	String countryOfOrigin, int birthday, int validFromDate, int expirationDate, char sex) {
+		this.surname = surname;
+		this.givenName = givenName; 
+		this.uscisNumber = uscisNumber;
+		this.countryOfOrigin = countryOfOrigin;
+		this.birthday = birthday;
+		this.validFromDate = validFromDate; 
+		this.expirationDate = expirationDate;
+		this.sex = sex;
+  }
   /**** CONSTRUCTORS ****/
   //TODO: Write the no-argument constructor and full constructor. Remember to include documentation for each method.
+/**
+ * Defualt Constructor sets all instance variables to defaults from lab requiremnts
+ * 
+ */
+  public DACArecipient()
+  {
+    /*surname = "Unknown";
+    givenName = "Unknwon"; 
+    uscisNumber = "Unknown";
+    countryOfOrigin = "Unkwown"; 
+    birthday = "2415021 ";
+    validFromDate = "2415021";
+    expirationDate = "2415021";
+    sex = 'X';*/
 
+    setAll(DEFAULT_SURNAME,"Unknown","Unknown","Unknown", 2415021, 2415021, 2415021,'X');
+  }
+
+  	public DACArecipient(String surname, String givenName, String uscisNumber, 
+	String countryOfOrigin, int birthday, int validFromDate, int expirationDate, char sex)
+	{
+		/*this.surname = surname;
+		this.givenName = givenName;
+		this.uscisNumber = uscisNumber;
+		this.countryOfOrigin = countryOfOrigin;
+		this.birthday = birthday;
+		this.validFromDate = validFromDate;
+		this.expirationDate = expirationDate;
+		this.sex = sex;*/
+
+		setAll(surname, givenName, uscisNumber, countryOfOrigin, birthday, validFromDate, expirationDate, sex);
+
+	}
 
 	/***** OTHER REQUIRED METHODS *****/
   public String toString()
--- Main.java
+++ Main.java
@@ -8,6 +8,13 @@ class Main
 {
   public static void main(String[] args) 
 	{
+      DACArecipient test = new DACArecipient();
 
+      System.out.println("Test: " + test);
+      //System.out.println("DACArecipient default");
+
+      DACArecipient test2 = new DACArecipient("Alejandro", "Bello", "123-456-789", "US", 2415021, 2415021, 2415021, 'M');
+
+      System.out.println("Test #2: " + test2);
   }
 }
